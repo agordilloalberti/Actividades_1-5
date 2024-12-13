9608e247747865a6672998f60f006fb071f50e14
Alvarogogal <agorsua0610@g.educaand.es>
2024-12-13 19:16:18 +0100

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.actividades_1_5"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.actividades_1_5"
--- app/src/main/java/com/example/actividades_1_5/screens/Actividades_1_5.kt
+++ app/src/main/java/com/example/actividades_1_5/screens/Actividades_1_5.kt
@@ -1,11 +1,12 @@
 package com.example.actividades_1_5.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -33,6 +35,7 @@ cuando se muestre la línea de progreso... Cuando pulsemos "Cancelar" vuelve al 
 @Composable
 fun Actividad1() {
     var showLoading by rememberSaveable { mutableStateOf(false) }
+    var buttonText by rememberSaveable { mutableStateOf("Cargar perfil") }
 
     Column(
         Modifier
@@ -43,6 +46,7 @@ fun Actividad1() {
     ) {
 
         if (showLoading) {
+            buttonText="Cancelar"
             CircularProgressIndicator(
                 color = Color.Red,
                 strokeWidth = 10.dp
@@ -52,12 +56,14 @@ fun Actividad1() {
                 color = Color.Red,
                 trackColor = Color.LightGray
             )
+        }else{
+            buttonText="Cargar Perfil"
         }
 
         Button(
             onClick = { showLoading = !showLoading }
         ) {
-            Text(text = "Cargar perfil")
+            Text(text = buttonText)
         }
     }
 }
@@ -68,7 +74,44 @@ Modifica ahora también que se separe el botón de la línea de progreso 30 dp,
 pero usando un estado... es decir, cuando no sea visible no quiero que tenga la separación
 aunque no se vea.
 */
+@Composable
+fun Actividad2() {
+    var showLoading by rememberSaveable { mutableStateOf(false) }
+    var buttonText by rememberSaveable { mutableStateOf("Cargar perfil") }
+    var space by rememberSaveable { mutableStateOf(30.dp)}
 
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        if (showLoading) {
+            buttonText="Cancelar"
+            space=30.dp
+            CircularProgressIndicator(
+                color = Color.Red,
+                strokeWidth = 10.dp,
+            )
+            LinearProgressIndicator(
+                modifier = Modifier.padding(top = 32.dp, bottom = space),
+                color = Color.Red,
+                trackColor = Color.LightGray
+            )
+        }else{
+            space=0.dp
+            buttonText="Cargar Perfil"
+        }
+
+        Button(
+            onClick = { showLoading = !showLoading }
+        ) {
+            Text(text = buttonText)
+        }
+    }
+}
 
 /*
 Actividad 3:
@@ -79,18 +122,27 @@ Actividad 3:
 */
 @Composable
 fun Actividad3() {
+    var progress by rememberSaveable { mutableFloatStateOf(0f) }
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        LinearProgressIndicator(progress = 0f)
+        LinearProgressIndicator(
+            progress = {progress},
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
 
-        Row(Modifier.fillMaxWidth()) {
-            Button(onClick = { /* TODO */ }) {
+        Row(Modifier.wrapContentWidth().align(Alignment.CenterHorizontally)) {
+            Button(
+                modifier = Modifier.alignByBaseline().padding(5.dp,15.dp),
+                onClick = {if (progress<1f) progress += 0.1f }
+            ) {
                 Text(text = "Incrementar")
             }
-            Button(onClick = { /* TODO */ }) {
+            Button(
+                modifier = Modifier.alignByBaseline().padding(5.dp,15.dp),
+                onClick = {if (progress>0.1f)progress-=0.1f}
+            ) {
                 Text(text = "Reducir")
             }
         }
@@ -108,11 +160,32 @@ y que no deje escribir más de un punto decimal...
 fun Actividad4() {
     var myVal by rememberSaveable { mutableStateOf("") }
 
-    TextField(
-        value = myVal,
-        onValueChange = { myVal = it },
-        label = { Text(text = "Importe") }
-    )
+    Box(modifier = Modifier.fillMaxSize().padding(20.dp)) {
+        TextField(
+            modifier = Modifier.align(Alignment.Center),
+            value = myVal,
+            onValueChange =
+            {
+                var s = it
+                s=s.replace(',','.')
+                var dec = false
+                s=s.filter { char ->
+                    if (char=='.'){
+                        if (dec){
+                            false
+                        }else {
+                            dec = true
+                            true
+                        }
+                    }else{
+                        true
+                    }
+                }
+                myVal = s
+            },
+            label = { Text(text = "Importe") }
+        )
+    }
 }
 
 
